f437c428242ad0177256cc662e345635e91cef34
Delmy <dfajardob@miumg.gt>
2025-03-01 08:02:44 -0600

    Busqueda lineal

---
+++ serie2/BUSQUEDA-LINEAL/BUSQUEDA-LINEAL/Busqueda_lineal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUSQUEDA_LINEAL
+{
+    class Busqueda_lineal
+    {//metodo que recibe un arreglo y un valor para  buscar
+        public static int LinearSearch(int[] arr, int valor)
+        { //recorre el arreglo para saber si el numero a buscar se encuentra 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == valor)
+                {
+                    //retorna elemento
+                    return i; 
+                }
+            }
+            // Retorna -1 si no lo encuentra
+            return -1; 
+        }
+
+    }
+}
---
+++ serie2/BUSQUEDA-LINEAL/BUSQUEDA-LINEAL/Program.cs
@@ -0,0 +1,40 @@
+﻿using BUSQUEDA_LINEAL;
+using System;
+
+public abstract class Program
+{ 
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenidos al programa de Busqueda Lineal ");
+        Console.WriteLine("Ingrese los elementos del arreglo separados por espacios: ");
+
+        string? input = Console.ReadLine();
+
+        // Si la entrada es null, asignamos una cadena vacía para evitar errores
+        int[] arr = (input ?? "").Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+        //validacion de arrgeglo vacio 
+        if (arr.Length == 0)
+        {
+            Console.WriteLine("No se ingresaron elementos en el arreglo");
+            return;
+        }
+        //solicita al usuario que ingrese el numero a buscar 
+        Console.Write("Ingrese el valor a buscar: ");
+        int valor = int.Parse(Console.ReadLine());
+
+        int result = Busqueda_lineal.LinearSearch(arr, valor);
+        //validacion de resultado final 
+        if (result != -1)
+        {
+            Console.WriteLine($"El número {valor} se encuentra en la posición {result}.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {valor} no está en el arreglo.");
+        }
+    }
+
+}
+    
